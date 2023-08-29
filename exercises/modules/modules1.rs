dca8f2046ad640aa243cccdd5c3bacaad88020c0
talves <tony@3alves.com>
2023-08-29 15:22:19 -0700

    finished: modules

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -3,17 +3,19 @@
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
-        get_secret_recipe();
+    pub fn make_sausage() {
+        // get_secret_recipe();
         println!("sausage!");
+        println!(
+            "Secret ingredient used in sausage factory is {}",
+            get_secret_recipe()
+        );
     }
 }
 
