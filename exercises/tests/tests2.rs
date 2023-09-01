393f40da434366433c42ae5d22d95cf1fecacd25
talves <tony@3alves.com>
2023-09-01 13:00:22 -0700

    finished: tests

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -6,12 +6,12 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let x = 1;
+        let y = &x;
+        assert_eq!(x, *y);
     }
 }
